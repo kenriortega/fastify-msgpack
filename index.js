9afc44dc11eadc8903446cb18a13546d1c72c3f0
kenriortega <kenriortega@gmail.com>
2021-05-16 21:06:06 -0400

    [START] first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const msgpack = require('@msgpack/msgpack')
+
+function msgpackSerializerPlugin(fastify, options, next) {
+    fastify.register(require('fastify-accepts-serializer'), {
+        serializers: [
+            {
+                regex: /^application\/msgpack$/,
+                serializer: body => Buffer.from(msgpack.encode(body))
+            }
+        ],
+        default: 'application/json'
+    })
+
+    fastify.addContentTypeParser('application/msgpack', {
+        parseAs: 'buffer'
+    }, async (req, body, done) => {
+        try {
+            const res = msgpack.decode(body)
+            return res
+        } catch (err) {
+            done(err)
+        }
+    })
+
+    next()
+}
+
+
+module.exports = fp(msgpackSerializerPlugin, {
+    fastify: '3.x',
+    name: 'fastify-msgpack',
+    dependencies: ['fastify-accepts-serializer']
+})

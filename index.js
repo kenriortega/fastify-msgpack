3ee5ec56f09b425ae84794891d2b472e75cb29f6
device <device@mail.com>
2021-05-17 11:57:58 -0400

    [REFACTOR]  github workflow and lint

--- index.js
+++ index.js
@@ -4,33 +4,34 @@ const fp = require('fastify-plugin')
 const msgpack = require('@msgpack/msgpack')
 
 function msgpackSerializerPlugin(fastify, options, next) {
-    fastify.register(require('fastify-accepts-serializer'), {
-        serializers: [
-            {
-                regex: /^application\/msgpack$/,
-                serializer: body => Buffer.from(msgpack.encode(body))
-            }
-        ],
-        default: 'application/json'
-    })
+  fastify.register(require('fastify-accepts-serializer'), {
+    serializers: [
+      {
+        regex: /^application\/msgpack$/,
+        serializer: body => Buffer.from(msgpack.encode(body))
+      }
+    ],
+    default: 'application/json'
+  })
 
-    fastify.addContentTypeParser('application/msgpack', {
-        parseAs: 'buffer'
-    }, async (req, body, done) => {
-        try {
-            const res = msgpack.decode(body)
-            return res
-        } catch (err) {
-            done(err)
-        }
-    })
+  fastify.addContentTypeParser('application/msgpack', {
+    parseAs: 'buffer'
+  }, async (req, body, done) => {
+    try {
+      const res = msgpack.decode(body)
+      return res
+    } catch (err) {
+      done(err)
+    }
+  })
 
-    next()
+  next()
 }
 
-
-module.exports = fp(msgpackSerializerPlugin, {
-    fastify: '3.x',
-    name: 'fastify-msgpack',
-    dependencies: ['fastify-accepts-serializer']
+const fastifyMsgpack = fp(msgpackSerializerPlugin, {
+  fastify: '3.x',
+  name: 'fastify-msgpack',
+  dependencies: ['fastify-accepts-serializer']
 })
+
+module.exports = fastifyMsgpack

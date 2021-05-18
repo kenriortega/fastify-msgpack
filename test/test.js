88dc3b91add6b413def0186281332c4349434c1c
kenriortega <kenriortega@gmail.com>
2021-05-17 21:23:43 -0400

    [CUT] remove un used import

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const t = require('tap')
 const test = t.test
 const plugin = require('../')
 const Fastify = require('fastify')
-const msgpack = require('@msgpack/msgpack')
+
 
 test('serializer', t => {
     t.plan(1)
